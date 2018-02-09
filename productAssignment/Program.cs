6e2678f4c83f7b65db96801d0289fec264383b73
ShrutiPromact <shruti@promactinfo.com>
2018-02-09 17:58:36 +0530

    commits

---
+++ productAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "Tomato",
+                "Potato",
+                "ToMato",
+                "Potato",
+                "Red Tomato",
+                "Tomato Catchup",
+                "Grapes",
+                "grapes",
+                "potato"
+            };
+            var check = list.ConvertAll(ar => ar.ToUpper());
+
+            int c = 0;
+
+            foreach (string word in check)
+            {
+                if (word == "TOMATO")
+
+                    c++;
+            }
+            Console.WriteLine("The Number of Tomato : " + c);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+
+                if (list[i] == "Potato")
+
+                    Console.WriteLine("The index of Potato is " + i);
+           }
+            Console.ReadKey();
+        }
+
+        
+       
+    }
+}
---
+++ summation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float X = 0.06f;
+            float Y = 5.94f;
+            float Z =X+Y;
+
+            Console.WriteLine("Summation of 2 numbers =" + (X + Y));
+
+            if (Z == 6)
+
+                Console.WriteLine("Equal");
+
+            else
+
+                Console.WriteLine("Not Equal");
+            Console.WriteLine();
+
+            Console.WriteLine("greatest value between X and y");
+
+            if (X > Y)
+
+                Console.WriteLine("X is greater!!");
+
+            else
+
+                Console.WriteLine("Y is greater!!");
+            Console.ReadKey();
+        }
+
+    }
+    }
+
